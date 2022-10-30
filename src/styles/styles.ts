f876a5c4a1244b4b7f82f6700f84f22a777abccb
ViniDevBR <vinigtr386@gmail.com>
2022-10-29 23:16:29 -0300

    :fire: New styles

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -4,20 +4,16 @@ export const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
-  background-color: #CACACA;
-
-  display: flex;
+  background-color: #8347B5;
+  background-image: linear-gradient(180deg, #8347B5, #242553);
   align-items: center;
   justify-content: center;
 ` 
 
 export const Content = styled.main`
-  background-image: linear-gradient(180deg, #8347B5, #242553);
-  border-radius: 1rem;
   width: 90%;
 `
 
 export const Row = styled.div`
   display: flex;
-  align-items: center;
 `
