f6da56f1f0b557fbc4bf967387146d887771ceea
ViniDevBR <vinigtr386@gmail.com>
2022-10-30 23:14:48 -0300

    :tada: New margin bottom

--- src/components/Display/styles.ts
+++ src/components/Display/styles.ts
@@ -2,13 +2,13 @@ import styled from "styled-components";
 
 export const DisplayContainer = styled.div`
   width: 100%;
-  
   display: flex;
   align-items: flex-end;
   justify-content: flex-end;
   font-size: 4rem;
   font-weight: 700;
   color: #FFFFFF;
+  margin-bottom: 3rem;
 
   input {
     width: 100%;
@@ -19,5 +19,6 @@ export const DisplayContainer = styled.div`
     font-weight: 700;
     color: #FFFFFF;
     text-align: end;
+    text-shadow: 2px 2px 2px #000;
   }
 `
